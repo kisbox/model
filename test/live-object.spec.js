kisbox/model

commit 835e12f837538fb332f04c0a781de828f3598940

logic Fix `$define` initial computation

src/live-object.js
@@ -5,7 +5,8 @@
 const { forArgs } = require("@kisbox/utils")
 
 const {
-  constructor: { shortcuts, call }
+  constructor: { shortcuts, call },
+  any: { isInstance }
 } = require("@kisbox/helpers")
 
 const Observable = require("./observable")
@@ -73,10 +74,12 @@ class LiveObject extends Observable {
 
   /* $define, $compute, $set */
   $define (key, depends, definition) {
-    const compute = function () {
+    const compute = () => {
       this[key] = definition.call(this, this)
     }
     this.$on(depends, compute)
+
+    if (isInstance(this)) compute()
 
     // TODO: Plutôt utiliser `trap`
     $events(this).put(`outdate:${key}`, compute)

test/live-object.spec.js
@@ -136,9 +136,11 @@ describe("liveObject", () => {
   describe(".$define()", () => {
     it("defines a dynamic property", () => {
       live.$define("foo", ["bar", "baz"], () => live.bar + live.baz)
-      expect(live.foo).toBe(undefined)
+      expect(live.foo).toEqual(NaN)
+
       live.bar = 2
       expect(live.foo).toEqual(NaN)
+
       live.baz = 3
       expect(live.foo).toBe(5)
       expect(Object.keys(live)).toEqual(["bar", "baz", "foo"])

commit 8ed5dd123fd3d790ae194cd3ebaa0c5881111489

dev Fix a regression in `$define()` context handling
(introduced by commit 835e12f)

src/live-object.js
@@ -74,12 +74,12 @@ class LiveObject extends Observable {
 
   /* $define, $compute, $set */
   $define (key, depends, definition) {
-    const compute = () => {
+    const compute = function () {
       this[key] = definition.call(this, this)
     }
     this.$on(depends, compute)
 
-    if (isInstance(this)) compute()
+    if (isInstance(this)) compute.call(this)
 
     // TODO: Plutôt utiliser `trap`
     $events(this).put(`outdate:${key}`, compute)

test/live-object.spec.js
@@ -189,6 +189,25 @@ describe("liveObject", () => {
       live.$define("foo", ["bar", "baz"], () => live.bar + live.baz)
       expect(Object.keys(live)).toEqual(["bar", "baz", "foo"])
     })
+
+    it("supports prototype-level definitions", () => {
+      class Square extends LiveObject {}
+      const proto = Square.prototype
+
+      proto.$define("area", ["length"], function (the) {
+        expect(the).toBe(instance)
+        expect(this).toBe(instance)
+        return the.length * the.length
+      })
+      expect(proto.area).toBe(undefined)
+
+      const instance = new Square()
+      instance.length = 10
+      expect(instance.length).toBe(10)
+      expect(instance.area).toBe(100)
+      expect(proto.length).toBe(undefined)
+      expect(proto.area).toEqual(undefined)
+    })
   })
 
   describe(".$set()", () => {